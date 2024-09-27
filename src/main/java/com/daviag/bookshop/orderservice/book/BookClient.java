a2a4c2012e28d3d205a971335a594d2b98dc67ad
David V. <villarejod@gmail.com>
2024-09-27 11:44:21 +0200

    timeout, retry & fallbacks

--- src/main/java/com/daviag/bookshop/orderservice/book/BookClient.java
+++ src/main/java/com/daviag/bookshop/orderservice/book/BookClient.java
@@ -3,7 +3,11 @@ package com.daviag.bookshop.orderservice.book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 @RequiredArgsConstructor
@@ -17,6 +21,11 @@ public class BookClient {
                 .get()
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                // externalize timeout in ClientProperties
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                .onErrorResume(WebClientResponseException.NotFound.class, e -> Mono.empty())
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+                .onErrorResume(Exception.class, e -> Mono.empty());
     }
 }
