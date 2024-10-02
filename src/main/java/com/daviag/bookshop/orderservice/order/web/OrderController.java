f1d488dac010d25b84889416aa49582ba193af04
David V. <villarejod@gmail.com>
2024-10-02 12:22:46 +0200

    retrieve orders for the authenticated user only

--- src/main/java/com/daviag/bookshop/orderservice/order/web/OrderController.java
+++ src/main/java/com/daviag/bookshop/orderservice/order/web/OrderController.java
@@ -4,6 +4,8 @@ import com.daviag.bookshop.orderservice.order.domain.Order;
 import com.daviag.bookshop.orderservice.order.domain.OrderService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,8 +18,8 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping
-    public Flux<Order> getAllOrders() {
-        return orderService.getAllOrders();
+    public Flux<Order> getAllOrders(@AuthenticationPrincipal Jwt jwt) {
+        return orderService.getAllOrders(jwt.getSubject());
     }
 
     @PostMapping
